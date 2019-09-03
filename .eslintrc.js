588fbce98c4991d6698e0f764534dc64889f6d99
RafaelLimaReis <rafaellimareis01@gmail.com>
2019-09-02 21:26:56 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/* https://eslint.org/docs/user-guide/configuring */
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true,
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {},
+    "plugins": [
+      "mocha"
+    ]
+};
